1230e8e437b1fc128ff75a2989d7137dff874fe8
cksghkd123 <cksghkd1234@naver.com>
2024-01-19 13:34:49 +0900

    .

--- src/main/java/com/example/signaling_server/handler/SignalHandler.java
+++ src/main/java/com/example/signaling_server/handler/SignalHandler.java
@@ -79,7 +79,7 @@ public class SignalHandler extends TextWebSocketHandler {
         String sender = signalData.getSender(); // origin of the message
         String data = signalData.getData(); // payload
         System.out.println("@@@@@@@@@@@@@@@@@@@@@@@");
-        System.out.println(signalData.getSignalType());
+        System.out.println(sender+" 로 부터 "+signalData.getSignalType());
         System.out.println("@@@@@@@@@@@@@@@@@@@@@@@");
 
         ChatRoom chatRoom;
@@ -208,13 +208,6 @@ public class SignalHandler extends TextWebSocketHandler {
             if (room != null) {
                 Map<String, WebSocketSession> clients = chatService.getClients(room);
 
-                /*
-                 * Map.Entry 는 Map 인터페이스 내부에서 Key, Value 를 쌍으로 다루기 위해 정의된 내부 인터페이스
-                 * 보통 key 값들을 가져오는 entrySet() 과 함께 사용한다.
-                 * entrySet 을 통해서 key 값들을 불러온 후 Map.Entry 를 사용하면서 Key 에 해당하는 Value 를 쌍으로 가져온다
-                 *
-                 * 여기를 고치면 1:1 대신 1:N 으로 바꿀 수 있지 않을까..?
-                 */
                 for (Map.Entry<String, WebSocketSession> client : clients.entrySet()) {
 
                     // send messages to all clients except current user
